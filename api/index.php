2798498b9f23658885c52d6db6a2f4c040b6e85f
ridhoduta <ridhodutay@gmail.com>
2026-02-05 19:53:02 +0700

    deploy again

--- api/index.php
+++ api/index.php
@@ -1,4 +1,12 @@
 <?php
 
+// Show all errors for debugging on Vercel
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Ensure the working directory is correct
+chdir(__DIR__ . '/../public');
+
 // Forward to CodeIgniter's index.php
 require __DIR__ . '/../public/index.php';
